9a3ba01ad4646cc06fe729fc8c631178055d436c
Jorge Lekuona <jorge.lekuona@opendeusto.es>
2021-01-10 12:05:49 +0100

    iniciando formato correcto

--- ProyectoIOT/__init__.py
+++ ProyectoIOT/__init__.py
@@ -22,23 +22,27 @@ GPIO.setmode(GPIO.BCM)
 
 # Pines de los servos
 servo_pin = 16          
-servo_pin1 = 19
-
-# Configurar pines Servos
-GPIO.setup(servo_pin, GPIO.OUT)     
-GPIO.setup(servo_pin1, GPIO.OUT)
-
-# Creacion de canales PWM en la frecuencia de 50Hz (En un principio no es necesario cambiar)
-p = GPIO.PWM(servo_pin, 50)
-p1 = GPIO.PWM(servo_pin1, 50)
-
-# Inicializar los servos en la posicion 0 -
-p.start(0)
-p1.start(0) 
+servo_pin_1 = 12
 
 # leds (los digitos son los numeros donde estan conectados los pines)
 led_0 = 5
 led_1 = 18
+
+# Inicializar pines botones
+boton_0 = 18
+boton_1 = 22
+
+# Configurar pines Servos
+GPIO.setup(servo_pin_0, GPIO.OUT)     
+GPIO.setup(servo_pin_1, GPIO.OUT)
+
+# Creacion de canales PWM en la frecuencia de 50Hz (En un principio no es necesario cambiar)
+p_0 = GPIO.PWM(servo_pin_0, 50)
+p_1 = GPIO.PWM(servo_pin_1, 50)
+
+# Inicializar los servos en la posicion 0 -
+p_0.start(0)
+p_1.start(0)
 
 # Configurar los pines de las Leds
 GPIO.setup(led_0, GPIO.OUT)
@@ -46,21 +50,16 @@ GPIO.output(led_0, GPIO.LOW)
 GPIO.setup(led_1, GPIO.OUT)
 GPIO.output(led_1, GPIO.LOW)
 
-# Inicializar pines botones
-boton_0 = 18
-boton_1 = 22
-
 # Configurar los pines de los botones
 GPIO.setup(boton_0, GPIO.IN)
 GPIO.setup(boton_1, GPIO.IN)
-
 
 #Pagina inicial de los leds
 @app.route('/leds')
 def home3():
     templateData = {
-        'led_0' : 5,
-        'led_1' : 18,
+        'led_0' : led_0,
+        'led_1' : led_1,
     }
     return render_template('led.html', **templateData)
 
@@ -86,23 +85,23 @@ def home2():
 @app.route("/test", methods=["POST"])
 def test():
     # Recoge los valore de los sliders
-    slider1 = request.form["slider1"]
-    slider2 = request.form["slider2"]
+    slider_0 = request.form["slider_0"]
+    slider_1 = request.form["slider_1"]
     # Cambia el ciclo para que el motor se mueva
-    p.ChangeDutyCycle(float(slider1))
-    p1.ChangeDutyCycle(float(slider2))
+    p_0.ChangeDutyCycle(float(slider_0))
+    p_1.ChangeDutyCycle(float(slider_1))
     # Espera a que se mueva
     sleep(1)
     # Para el servo
-    p.ChangeDutyCycle(0)
-    p1.ChangeDutyCycle(0)
+    p_0.ChangeDutyCycle(0)
+    p_1.ChangeDutyCycle(0)
     return render_template('servo.html')
 
 
 # Inicializa la ventana de los botones
 @app.route("/botones")
 def hello():
-    if GPIO.input(22):
+    if GPIO.input(boton_0):
         doorStatus = "open"
     else:
         doorStatus = "closed"
@@ -132,9 +131,21 @@ def view_home():
 
 @app.route("/reacciones")
 def reacciones():
-    return render_template("reacciones.html", title="reacciones")
+    if GPIO.input(boton_0):
+        respuesta = "Hola"
+    elif GPIO.input(boton_1):
+        respuesta = "Estoy bien"
+    elif GPIO.input(boton_2):
+        respuesta = "Que tal?"
+    templateData = {
+        'respuesta': respuesta,
+    }
+    return render_template("reacciones.html", title="reacciones", **templateData)
 
 
 @app.route("/pickles")
 def pickles():
     return render_template("pickles.html", title="pickles")
+
+@app.route("/saludo")
+def saludo():
